c91dfc057ce0335da36a0f6a0680119128a19dd8
Juan Ospina <silohe6@gmail.com>
2020-05-07 11:45:07 -0500

    actualizacion de punto y coma

--- clase1.js
+++ clase1.js
@@ -1,9 +1,9 @@
-var nombre = 'Sacha', apellido = 'Lifszyc'
-var edad = 28
+var nombre = 'Sacha', apellido = 'Lifszyc';
+var edad = 28;
 
-edad = '28 años'
+edad = '28 años';
 
-console.log('Hola ' + nombre + ' ' + apellido)
-console.log('Tengo ' + edad)
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad);
 
-var peso = 75
+var peso = 75;
